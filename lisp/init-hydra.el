98f509923625481fb1224604ea4a9b37fa57844a
jmgpena <jorge@jmgpena.net>
2015-04-21 23:12:21 +0100

    Added hydra-rectangle recipe

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -33,4 +33,33 @@ _~_: modified
 
     (define-key Buffer-menu-mode-map "." 'hydra-buffer-menu/body)))
 
+;;; rectangle mode hydra
+(defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
+                                     :color pink
+                                     :post (deactivate-mark))
+ "
+  ^_k_^     _d_elete    _s_tring
+_h_   _l_   _o_k        _y_ank
+  ^_j_^     _n_ew-copy  _r_eset
+^^^^        _e_xchange  _u_ndo
+^^^^        ^ ^         _p_aste
+"
+  ("h" backward-char nil)
+  ("l" forward-char nil)
+  ("k" previous-line nil)
+  ("j" next-line nil)
+  ("e" exchange-point-and-mark nil)
+  ("n" copy-rectangle-as-kill nil)
+  ("d" delete-rectangle nil)
+  ("r" (if (region-active-p)
+           (deactivate-mark)
+         (rectangle-mark-mode 1)) nil)
+  ("y" yank-rectangle nil)
+  ("u" undo nil)
+  ("s" string-rectangle nil)
+  ("p" kill-rectangle nil)
+  ("o" nil nil))
+(global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
+
 (provide 'init-hydra)
+;;; init-hydra ends here
