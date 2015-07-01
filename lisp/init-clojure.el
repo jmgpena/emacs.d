bda87df6ad202396385f0535752f95ca51a547ad
Pena, Jorge <Jorge.Pena@paddypower.com>
2015-07-01 13:50:23 +0100

    Change anonymous lambda char in clojure mode

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -20,7 +20,7 @@
   (font-lock-add-keywords
    'clojure-mode `(("\\(#\\)("
                     (0 (progn (compose-region (match-beginning 1)
-                                              (match-end 1) "ƒ")
+                                              (match-end 1) "λ")
                               nil)))))
   (font-lock-add-keywords
    'clojure-mode `(("\\(#\\){"
