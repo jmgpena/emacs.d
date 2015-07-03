63bfc645bac2ba0556c400d3c512574c8b58d287
Pena, Jorge <Jorge.Pena@paddypower.com>
2015-07-03 09:20:06 +0100

    Update magit configs for new version 2.1

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -21,8 +21,6 @@
 ;;;----------------------------------------------------------------------------
 (require-package 'magit)
 (require-package 'git-blame)
-(require-package 'git-commit-mode)
-(require-package 'git-rebase-mode)
 (require-package 'gitignore-mode)
 (require-package 'gitconfig-mode)
 (require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
@@ -32,7 +30,8 @@
  magit-save-some-buffers nil
  magit-process-popup-time 10
  magit-diff-refine-hunk t
- magit-completing-read-function 'magit-ido-completing-read)
+ ;magit-completing-read-function 'magit-ido-completing-read
+ )
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
@@ -51,10 +50,6 @@
 ;;(after-load 'session
 ;;  (add-to-list 'session-mode-disable-list 'git-commit-mode))
 
-;;; When we start working on git-backed files, use git-wip if available
-(after-load 'magit
-  (global-magit-wip-save-mode))
-
 (when *is-a-mac*
   (after-load 'magit
     (add-hook 'magit-mode-hook (lambda () (local-unset-key [(meta h)])))))
@@ -63,12 +58,12 @@
 (global-set-key (kbd "C-x v f") 'vc-git-grep)
 
 ;;; git-svn support
-(require-package 'magit-svn)
-(autoload 'magit-svn-enabled "magit-svn")
-(defun site/maybe-enable-magit-svn-mode ()
-  (when (magit-svn-enabled)
-    (magit-svn-mode)))
-(add-hook 'magit-status-mode-hook #'site/maybe-enable-magit-svn-mode)
+;(require-package 'magit-svn)
+;(autoload 'magit-svn-enabled "magit-svn")
+;(defun site/maybe-enable-magit-svn-mode ()
+;  (when (magit-svn-enabled)
+;    (magit-svn-mode)))
+;(add-hook 'magit-status-mode-hook #'site/maybe-enable-magit-svn-mode)
 
 (after-load 'compile
   (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
